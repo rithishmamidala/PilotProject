75a15adef42d8cd42d05922c1329f2695fb56698
rithishmamidala <20r21a1287@mlrinstitutions.ac.in>
2024-08-03 12:38:34 +0530

    v1

---
+++ PilotCloud/src/test/java/com/TeamC/PilotCloud/PilotCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeamC.PilotCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
